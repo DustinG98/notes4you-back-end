dc6a7fdae26764b3a815d4e9943bdde8227ddc50
DustinG98 <thedustingraham@gmail.com>
2020-01-14 20:31:25 -0700

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 5000;
 const corsOptions = {
     exposedHeaders: ['auth-token', 'user_id']
 }
 
 app.use(cors(corsOptions))
+
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json())
 
 const uri = process.env.ATLAS_URI
