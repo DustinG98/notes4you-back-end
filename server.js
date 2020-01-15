857a50fe38bfb50d1fe4fed54fe3e227392bcbe0
DustinG98 <thedustingraham@gmail.com>
2020-01-14 21:20:49 -0700

    updated cors options???

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-    origin: [/notes4you.netlify\.com$/],
+    origin: "*",
     exposedHeaders: ['auth-token', 'user_id']
 }
 
